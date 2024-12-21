afecf1fab5b2f2428a996a4fe267fdcf10e78308
Lucy Hühn <666nikkineko@gmail.com>
2024-12-21 11:14:36 +0100

    init project

---
+++ src/main/java/com/xenya52/fmc003_rest_api/Fmc003RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.xenya52.fmc003_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fmc003RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fmc003RestApiApplication.class, args);
+	}
+
+}

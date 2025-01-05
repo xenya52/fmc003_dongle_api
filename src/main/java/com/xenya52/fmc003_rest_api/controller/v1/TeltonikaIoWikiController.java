7c7dfd9e40ceb12c52bc6add6cf0fa6c55104640
Lucy Hühn <666nikkineko@gmail.com>
2025-01-05 20:11:16 +0100

    removed scrape from teltonika, prep MongoDB - removed teltonika scrape class and the IoWikiResponse model because of the HTTP error 403 from teltonika - add mongoDB for springboot and dotenv for java to pom.xml - Create .env file and a .envexample for the user, this will be important in the future when im writing my Setup section in the readme - Add Dcokerfiles for building my project in docker and a compose for additionaly mongoDB use - Add MongoDBConfig to my springboot project - TODO Make the connection the the mongoDB, Support the TeltonikaWikiController Restapis with the datafragment that I keept from the site

--- src/main/java/com/xenya52/fmc003_rest_api/controller/v1/TeltonikaIoWikiController.java
+++ src/main/java/com/xenya52/fmc003_rest_api/controller/v1/TeltonikaIoWikiController.java
@@ -1,29 +1,15 @@
 package com.xenya52.fmc003_rest_api.controller.v1;
 
-import com.xenya52.fmc003_rest_api.service.ScrapeTeltonikaIoWiki;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("teltonika-io-data")
+@RestController("teltonikaIoWiki")
 public class TeltonikaIoWikiController {
 
-    // Attributes
-    private ScrapeTeltonikaIoWiki ioWiki;
-
-    // Constructors
-    public TeltonikaIoWikiController() {
-        this.ioWiki = new ScrapeTeltonikaIoWiki();
-    }
-
     // Methods
-    @GetMapping("/teltonika-io-data")
+    @GetMapping("/")
     public String getTeltonikaIoPage() {
         return ioWiki.getDataSendingParameters();
-    }
-
-    @GetMapping("/teltonika-io-data-json")
-    public String getTeltonikaIoPageJson() {
-        return ioWiki.getDataSendingParametersJson();
     }
 
     @GetMapping("/iowiki-id-to-name")
--- src/main/java/com/xenya52/fmc003_rest_api/entity/dto/README.md
+++
@@ -1,4 +0,0 @@
-Node for myself (delete later)
-
-# DTOs
-Are Data Transfer Objects. They are used to transfer data between the client and the server. They are also used to transfer data between different layers of the application. They are used to encapsulate the data that needs to be transferred. The difference between a modle and a dto is that a model is used to represent the data in the database, while a dto is used to represent the data that needs to be transferred between the client and the server.

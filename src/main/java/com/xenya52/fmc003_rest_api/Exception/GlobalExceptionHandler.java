c65cffdbf65a36a87f21e4433333baee774d058d
Lucy Hühn <666nikkineko@gmail.com>
2025-03-16 20:35:44 +0100

    add logging for GlobalExceptionHandler - Heres the issue - TODO: Fix that issue

--- src/main/java/com/xenya52/fmc003_rest_api/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/xenya52/fmc003_rest_api/Exception/GlobalExceptionHandler.java
@@ -1,18 +1,26 @@
 package com.xenya52.fmc003_rest_api.Exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+Heres the issue
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(
+        GlobalExceptionHandler.class
+    );
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(
         IllegalArgumentException ex
     ) {
+        logger.error("IllegalArgumentException: ", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
@@ -20,11 +28,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleOptimisticLockingFailureException(
         OptimisticLockingFailureException ex
     ) {
+        logger.error("OptimisticLockingFailureException: ", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
+        logger.error("Exception: ", ex);
         return new ResponseEntity<>(
             ex.getMessage(),
             HttpStatus.INTERNAL_SERVER_ERROR
